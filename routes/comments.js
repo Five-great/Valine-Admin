9c4e59a89dbc390dc2076dbaa21be06a44df2324
Five-great <43601963+Five-great@users.noreply.github.com>
2019-10-31 23:23:32 +0800

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -8,53 +8,39 @@ router.get('/', function (req, res, next) {
     if (req.currentUser) {
         let query = new AV.Query(Comment);
         let qShopdata = new AV.Query(stop_data);
+        let q1;
         query.descending('createdAt');
         qShopdata.descending('createdAt');
         qShopdata.limit(50);
-        //query.limit(50);
+        query.limit(50);
        
        
  
-        /*
+    
          query.find().then(function (results) {
-            res.render('comments', {
-                title: process.env.SITE_NAME + '上的新评论',
-                domain: process.env.SITE_URL,
-                comment_list: results
+           q1 = results;
+        }, 
+        qShopdata.find().then(function (results) {
+             console.log(results);
+             res.render('comments', {
+                    title: process.env.SITE_NAME + '上的新评论',
+                    domain: process.env.SITE_URL,
+                    comment_list: q1,
+                    stop_data_list: results
             });
-        }, function (err) {
+        }, 
+         function (err) {
             if (err.code === 101) {
                 res.render('comments', {
                     title: process.env.SITE_NAME + '上的新评论',
                     domain: process.env.SITE_URL,
-                    comment_list: []
+                    comment_list: [],
+                    stop_data_list: []
                 });
             } else {
                 next(err);
             } 
         }).catch(next);
-        */
-        
-         qShopdata.find().then(function (results) {
-             console.log(results);
-             res.render('comments', {
-                     title: process.env.SITE_NAME + '上的新评论',
-                    domain: process.env.SITE_URL,
-                stop_data_list: results
-            });
-        }, function (err) {
-            if (err.code === 101) {
-                res.render('comments', {
-                    title: process.env.SITE_NAME + '上的新评论',
-                    domain: process.env.SITE_URL,
-                   stop_data_list: []
-                });
-            } else {
-                next(err);
-            }
-        }).catch(next);
-        
-        
     } else {
         res.redirect('/');
     }
